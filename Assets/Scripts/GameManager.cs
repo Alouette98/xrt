8e7020ee17213680bdbc5c620597193e9d1b1dd6
Alouette98 <36831940+Alouette98@users.noreply.github.com>
2023-02-14 19:02:40 -0500

    Minor update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,13 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     public static GameManager instance;
     public sliderMenuAnim m_PanelDown;
+    public AppState m_AppState;
+
+    public enum AppState
+    {
+        Standby,
+        Selection,
+    }
 
     void Awake()
     {
