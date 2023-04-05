d2fed2561d099b8fd66adf21805ee97d3ab5e2f9
Alouette98 <36831940+Alouette98@users.noreply.github.com>
2023-04-05 17:13:47 -0400

    Update scenes in login

---
+++ Assets/Scripts/Login/LoginButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginButton : MonoBehaviour
+{
+    public GameObject LoginCanvas;
+
+    public GameObject WelcomeCanvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void LoginButtonBehaviour()
+    {
+        // Debug.LogWarning("--Login button clicked--");
+        LoginCanvas.SetActive(true);
+        WelcomeCanvas.SetActive(false);
+    }
+}
